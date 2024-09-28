243b7b2069adf5ad7e5203d90a588ca41d6dce43
shashivipul <71481041+shashivipul@users.noreply.github.com>
2024-09-29 00:23:41 +0530

    Add files via upload

---
+++ eva_fair.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def fair_metric(pred, labels, sens):
+    idx_s0 = sens==0
+    idx_s1 = sens==1
+    idx_s0_y1 = np.bitwise_and(idx_s0, labels==1)
+    idx_s1_y1 = np.bitwise_and(idx_s1, labels==1)
+    parity = abs(sum(pred[idx_s0])/sum(idx_s0)-sum(pred[idx_s1])/sum(idx_s1))
+    equality = abs(sum(pred[idx_s0_y1])/sum(idx_s0_y1)-sum(pred[idx_s1_y1])/sum(idx_s1_y1))
+    return parity.item(), equality.item()
